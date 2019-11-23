a8006e4153e84c416020d8318a4b424fc0c323e8
Christian Munklinde Leth-Espensen <201805807@post.au.dk>
2019-11-23 02:54:24 +0100

    GUI

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
         System.out.println(Haversine.distance(56.17203083, 10.18773750, 56.17206200, 10.18766933)*1000);
         Coordinate coordinate = Haversine.inverseHaversine(new Coordinate(56.17203083, 10.18773750 ), 90.00000, 0.000000);
         System.out.println(Haversine.distance(coordinate.getLatitude(), coordinate.getLongitude(), 56.17206200, 10.18766933)*1000);
-       // FileLoader fileLoader = new FileLoader();
-        new GUI();
-
- /*       List<Coordinate> coordinates = fileLoader.getCoordinatesLoadedCoordinates();
+        GUI gui = new GUI();
+        gui.update(true);
+        FileLoader fileLoader = new FileLoader();
+        List<Coordinate> coordinates = fileLoader.getCoordinatesLoadedCoordinates();
         System.out.println(coordinates.get(0).getLatitude()+", " + coordinates.get(0).getLongitude());
         Square square = new Square(coordinates);
         System.out.println(square);
         System.out.println(square.isContained(new Object(new Coordinate(56.17202983, 10.18773750 ), 1, -90)));
-*/
+
     }
 }
